75491f692b003836088112642e7dce62bdeff932
jtrieu1998 <43070404+jtrieu1998@users.noreply.github.com>
2025-06-08 23:02:46 -0500

    button top right aligned

---
+++ components/AddBedButton.tsx
@@ -0,0 +1,41 @@
+// import Ionicons from "@expo/vector-icons/Ionicons";
+import { View, StyleSheet, Button, TouchableHighlight, Alert } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+
+export default function App() {
+  return (
+    <View style={styles.plusButtonContainer}>
+      <TouchableHighlight
+        onPress={createTwoButtonAlert}
+        style={styles.container}
+      >
+        <AntDesign name="pluscircleo" size={40} color="black" />
+      </TouchableHighlight>
+    </View>
+  );
+}
+
+const createTwoButtonAlert = () =>
+  Alert.alert("Alert Title", "My Alert Msg", [
+    {
+      text: "Cancel",
+      onPress: () => console.log("Cancel Pressed"),
+      style: "cancel",
+    },
+    { text: "OK", onPress: () => console.log("OK Pressed") },
+  ]);
+
+const styles = StyleSheet.create({
+  container: {
+    // backgroundColor: "green",
+    alignSelf: 'flex-end',
+    padding:10,
+  },
+  plusButtonContainer: {
+    display: 'flex',
+    justifyContent:'center',
+    flex: 1,
+    backgroundColor: "blue",
+  },
+});
